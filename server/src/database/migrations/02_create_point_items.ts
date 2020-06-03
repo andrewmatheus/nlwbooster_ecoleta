32947067091453cf63ca13cc898c5ea518419ae0
Andrew Matheus <andrewmatheus@gmail.com>
2020-06-02 21:06:22 -0300

    2 dia, faltando arrumar compatibilidade

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,7 @@
+export async function up(){
+  //Criar a tabela
+}
+
+export async function down(){
+  //Deletar a tabela
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 
-const app = express();
+import routes from './routes';
 
-app.get('/users', (request, response) => {
-  return response.json([
-    'Diego',
-    'Andrew',
-    'Surama',
-    'Miguel'
-  ]);
-})
+const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
